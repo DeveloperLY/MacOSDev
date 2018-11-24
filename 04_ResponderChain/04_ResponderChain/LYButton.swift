89f03a859ec4a984088e73a8ca3bb467ee707cfc
LY <coderyliu@gmail.com>
2018-11-24 21:15:08 +0800

    Add ResponderChain

---
+++ 04_ResponderChain/04_ResponderChain/LYButton.swift
@@ -0,0 +1,23 @@
+//
+//  LYButton.swift
+//  04_ResponderChain
+//
+//  Created by LiuY on 2018/11/24.
+//  Copyright © 2018 DeveloperLY. All rights reserved.
+//
+
+import Cocoa
+
+class LYButton: NSButton {
+    
+    override func scrollWheel(with event: NSEvent) {
+        print("scrollWheel \(self)")
+    }
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
